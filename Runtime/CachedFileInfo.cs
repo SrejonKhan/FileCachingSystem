75d696b5dd937c198f7bed3b0026b9b01a68dd46
Srejon Khan <srejonkhan79@gmail.com>
2021-09-08 22:02:02 +0600

    initial commit

---
+++ Runtime/CachedFileInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace FileCachingSystem
+{
+    public class CachedFileInfo
+    {
+        public string id;
+        public string path;
+        public string hash;
+    }
+}
---
+++ Runtime/FileCacher.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Serialization;
+
+namespace FileCachingSystem
+{
+    public class FileCacher
+    {
+        public static void Load()
+        {
+
+        }
+
+        public static void Save()
+        {
+
+        }
+    }
+}
